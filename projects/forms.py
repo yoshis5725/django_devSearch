dd48dafba60e3a397a5190014e29a4df3d9dac91
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2023-03-09 05:36:16 -0700

    Theme installation for the Add Project form - left the template in the template root directory - will remove later.

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -7,5 +8,14 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'description', 'featured_image', 'demo_link', 'source_link', 'tags']
 
+        # customizing/overwriting the classes
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'input'}),
+            'description': forms.Textarea(attrs={'class': 'input'}),
+            'featured_image': forms.FileInput(attrs={'class': 'input'}),
+            'demo_link': forms.TextInput(attrs={'class': 'input'}),
+            'source_link': forms.TextInput(attrs={'class': 'input'}),
+            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'input'}),
+        }
 
 
